9124e8818c299572548b59793836143473103ea6
NuBerry <h.midtun@hotmail.com>
2020-03-29 22:10:51 +0200

    added TrondheimPage

--- app/view.py
+++ app/view.py
@@ -3,10 +3,49 @@ from controller import TrondelagController
         
 class Page:
     def __init__(self, root=None):
-        self.frame = Frame(root)
-        self.frame.pack()
-    
-    
+        self.frame = Frame(
+            root, bg='black')
+        self.frame.pack_propagate(0)
+        self.frame.pack(fill=BOTH, expand=1)
+
+class TrondheimPage(Page):
+    def __init__(self, master):
+        Page.__init__(self)
+        self.controller = TrondelagController()
+        self.__set_layout()
+        
+    def __city_info(self, name=None, rank=0):
+        city_data = self.controller.city_data(name=name, rank=rank)
+        
+        name = city_data['navn']
+        name_label = Label(
+            self.frame, text=name, fg='white', bg='black',
+            font=("Helvetica",50),
+            )
+        name_label.pack()
+        
+        infected = city_data['smittet']
+        dead = city_data['dode']
+        stats_label = Label(
+            self.frame,
+            text="{}/{}".format(infected,dead),
+            fg='white', bg='black',
+            font=("Helvetica",100)
+            )
+        stats_label.pack()
+         
+
+    def __set_layout(self):
+        self.__city_info('Trondheim')
+        
+        last_update = self.controller.last_updated()
+        update_label = Label(
+            self.frame, text=last_update.strftime("%H:%M:%S"),
+            fg='white', bg='black',
+            font=("Helvetica",30)
+            )
+        update_label.pack(side=BOTTOM)
+
 class TopCasesPage(Page):
     def __init__(self, master):
         Page.__init__(self)
@@ -24,16 +63,11 @@ class TopCasesPage(Page):
         name_label.pack()
         
         infected = city_data['smittet']
-        infected_label = Label(
-            self.frame, text="Smittede: {}".format(infected)
-            )
-        infected_label.pack()
-        
         dead = city_data['dode']
-        dead_label = Label(
-            self.frame, text="Døde: {}".format(dead)
+        stats_label = Label(
+            self.frame, text="{}/{}".format(infected,dead)
             )
-        dead_label.pack()
+        stats_label.pack()
         
 
         
@@ -59,10 +93,6 @@ class LandingPage(Page):
         self.__set_layout()
     
     def __set_layout(self):
-        self.label = Label(
-            self.frame, text='COVID-19'
-            )
-        self.label.pack()
         
         self.button = Button(
             self.frame, text='QUIT', fg="red", command=self.frame.quit
@@ -72,14 +102,27 @@ class LandingPage(Page):
         self.top_cases_button = Button(
             self.frame, text='Top Cases', command=self.__top_cases
             )
-        self.top_cases_button.pack(side=RIGHT)
+        self.top_cases_button.pack(side=LEFT)
+        
+        self.trondheim_button = Button(
+            self.frame, text='Trondheim', command=self.__trondheim
+            )
+        self.trondheim_button.pack(side=LEFT)
         
     def __top_cases(self):
         top_cases = TopCasesPage(self.frame.master)
+        self.frame.destroy()
+    
+    def __trondheim(self):
+        trondheim = TrondheimPage(self.frame.master)
         self.frame.destroy()
 
 class App():
     def __init__(self):
         self.root = Tk()
+        self.root.geometry("500x500")
+        self.root.title("COVID-19 // TRD")
         landing_page = LandingPage(self.root)
         self.root.mainloop()
+        
+app=App()
