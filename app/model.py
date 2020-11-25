7c7769e064ffed2aaeada8253d7826cc7f8b214f
nufon <h.midtun@hotmail.com>
2020-11-25 18:40:29 +0100

    JSONDecodeError

--- app/model.py
+++ app/model.py
@@ -4,6 +4,7 @@
 '''
 
 import requests
+import json
 import dateutil.parser
 from datetime import timedelta
 
@@ -20,15 +21,20 @@ class COVID:
         try:
             print("Requesting data from Adressa...")
             r = requests.get(url= "https://" + self.url, headers={"Authorization":"B9329CEF38B2F"})
-            raw_data = r.json()
-            if r != None:
-                print("Updating {} data...".format(self.__class__.__name__))
-                self.data = self._prepare_data(raw_data)
-                iso_time = raw_data['data']['updated']
-                last_update = dateutil.parser.isoparse(iso_time)
-                self.last_updated = last_update + timedelta(hours=1)
         except requests.exceptions.RequestException as e:
             print(e)
+            return
+        try:
+            raw_data = r.json()
+        except json.JSONDecodeError as e:
+            print(e)
+            print(r.content)
+            return
+        print("Updating {} data...".format(self.__class__.__name__))
+        self.data = self._prepare_data(raw_data)
+        iso_time = raw_data['data']['updated']
+        last_update = dateutil.parser.isoparse(iso_time)
+        self.last_updated = last_update + timedelta(hours=1)
 
     def _prepare_data(self, raw_data):
         raw_data = raw_data['data']['data']
